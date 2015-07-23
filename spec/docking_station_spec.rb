eb8db268b156b526574451bbf47d391c545e0d4c
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-07-23 22:58:50 +0100

    all tests passing - docking station will only release working bikes and will not release broken bikes

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -8,7 +8,16 @@ describe DockingStation do
 	it 'should initialize with a capacity of 50 if user inputs 50 as an argument into DockingStation.new' do
 		expect(DockingStation.new(50).capacity).to eq(50)
 	end
-		
+	
+	it {is_expected.to respond_to(:dock).with(1).argument}
+
+	describe '#dock' do 
+		it 'raises an error when full' do
+			subject.capacity.times { subject.dock Bike.new }
+			expect { subject.dock Bike.new }.to raise_error 'Docking station full'
+		end
+	end
+	
 	it {is_expected.to respond_to :release_bike}
 
 	it "releases a Bike after a bike has been docked" do
@@ -21,33 +30,38 @@ describe DockingStation do
 		expect(subject.release_bike).to be_working
 	end
 
+	describe "#release_bike" do
+		it "fails if docking station is empty" do
+			expect{subject.release_bike}.to raise_error("No bikes available!")
+		end
+	end
+
 	context 'only one broken bike is docked' do 
 		bike = Bike.new
 		bike.report_broken
-		broken_bike = bike
 		station = DockingStation.new
-		station.dock(broken_bike)
+		station.dock bike
 
 		describe "#release_bike" do
-			it 'fails if no working bikes are available' do 
+			it 'fails and raises an exception' do 
 			expect{ station.release_bike }.to raise_error("No working bikes available!")
 			end
 		end
 	end
 
-	context 'one broken bike and one working bike are docked' do
-		station = DockingStation.new
-		working_bike = Bike.new
-		station.dock working_bike
-		broken_bike = Bike.new
-		broken_bike.report_broken
-		station.dock broken_bike
+	# context 'one broken bike and one working bike are docked' do
+	# 	station = DockingStation.new
+	# 	working_bike = Bike.new
+	# 	station.dock working_bike
+	# 	broken_bike = Bike.new
+	# 	broken_bike.report_broken
+	# 	station.dock broken_bike
 
-		it 'releases the working bike and then will not release the broken bike' do 
-			expect(station.release_bike).to eq(working_bike)
-			expect{station.release_bike}.to raise_error("No working bikes available!")
-		end
-	end
+	# 	it 'releases the working bike and then will not release the broken bike' do 
+	# 		expect(station.release_bike).to eq(working_bike)
+	# 		expect{station.release_bike}.to raise_error("No working bikes available!")
+	# 	end
+	# end
 
 	context 'a mixture of working bikes and broken bikes are docked' do 
 		station = DockingStation.new
@@ -56,32 +70,17 @@ describe DockingStation do
 		broken_bike1 = Bike.new
 		broken_bike1.report_broken
 		broken_bike2 = Bike.new
-		broken_bike1.report_broken
+		broken_bike2.report_broken
 		station.dock working_bike1 
 		station.dock broken_bike1
 		station.dock working_bike2 
 		station.dock broken_bike2
 
 		it 'releases all working bikes first but then will not release anymore bikes' do 
-			expect(station.release_bike).to eq(working_bike1)
-			expect(station.release_bike).to eq(working_bike2)
+			expect(station.release_bike).to be_working
+			expect(station.release_bike).to be_working
 			expect{station.release_bike}.to raise_error("No working bikes available!")
 		end
 	end
-
-	describe "#release_bike" do
-		it "fails if no bikes available" do
-			expect{subject.release_bike}.to raise_error("No bikes available!")
-		end
-	end
-
-	it {is_expected.to respond_to(:dock).with(1).argument}
-
-	describe '#dock' do 
-		it 'raises an error when full' do
-			subject.capacity.times { subject.dock Bike.new }
-			expect { subject.dock Bike.new }.to raise_error 'Docking station full'
-		end
-	end	
 
 end
