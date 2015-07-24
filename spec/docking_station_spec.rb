c66b2e1f112e2019a79850470e7834cc297c6422
Andrew Stelmach <andrew_stelmach@hotmail.com>
2015-07-24 13:59:46 +0100

    complete post-challenge 4

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -13,8 +13,8 @@ describe DockingStation do
 
 	describe '#dock' do 
 		it 'raises an error when full' do
-			subject.capacity.times { subject.dock Bike.new }
-			expect { subject.dock Bike.new }.to raise_error 'Docking station full'
+			subject.capacity.times { subject.dock double :bike }
+			expect { subject.dock double :bike }.to raise_error 'Docking station full'
 		end
 	end
 
@@ -25,29 +25,38 @@ describe DockingStation do
 		expect(subject.release_bike).to be_instance_of(Bike)
 	end
 
-	it "releases a working bike" do 
-		subject.dock(Bike.new)
-		expect(subject.release_bike).to be_working
+	it "releases a working bike" do
+		bike = double :bike, working?: true 
+		subject.dock bike
+		expect(subject.release_bike).to be bike
 	end
 
 	describe "#release_bike" do
 		it "fails if docking station is empty" do
 			expect{subject.release_bike}.to raise_error("No bikes available!")
 		end
-	end
 
-	context 'when only one broken bike is docked' do 
-		bike = Bike.new
-		bike.report_broken
-		station = DockingStation.new
-		station.dock bike
-
-		describe "#release_bike" do
-			it 'fails and raises an exception' do 
-			expect{ station.release_bike }.to raise_error("No working bikes available!")
-			end
+		it 'does not release broken bikes' do 
+			subject.dock double :bike, working?: false
+			#subject.dock double :bike, working?: false
+			expect { subject.release_bike }.to raise_error("No working bikes available!")
 		end
 	end
+
+	#I want to be able to setup the below test with doubles!
+
+	# context 'when only one broken bike is docked' do 
+	# 	bike = Bike.new
+	# 	bike.report_broken
+	# 	station = DockingStation.new
+	# 	station.dock bike
+
+	# 	describe "#release_bike" do
+	# 		it 'fails and raises an exception' do 
+	# 		expect{ station.release_bike }.to raise_error("No working bikes available!")
+	# 		end
+	# 	end
+	# end
 
 	context 'when a mixture of working bikes and broken bikes are docked' do 
 		station = DockingStation.new
